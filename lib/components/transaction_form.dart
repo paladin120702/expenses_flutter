0edf3be7a8031301746468f1f2f70caba7df6885
devartur <arturvinicius.estudante.trabalho@gmail.com>
2025-04-07 21:20:35 -0300

    Adicionando novas cores, fontes e imagens a aplicação

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
+class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
 
   TransactionForm(this.onSubmit);
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
 
   _submitForm() {
     final title = titleController.text;
@@ -16,7 +21,7 @@ class TransactionForm extends StatelessWidget {
       return;
     }
 
-    onSubmit(title, value);
+    widget.onSubmit(title, value);
   }
 
   @override
@@ -47,6 +52,7 @@ class TransactionForm extends StatelessWidget {
               child: Text('Nova Transação'),
               style: ElevatedButton.styleFrom(
                 foregroundColor: Colors.purple,
+                backgroundColor: Colors.white,
               ),
             ),
           ],
